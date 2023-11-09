3ff810fbfbcef68f7009b778d52c3e1be6697ada
rubenbalbastre <96421412+rubenbalbastre@users.noreply.github.com>
2023-11-09 19:59:12 +0100

    refactor pre-processing datasets EMR

--- src/utils/utils.jl
+++ src/utils/utils.jl
@@ -15,35 +15,12 @@ end
 function import_project_utils(utils_path)
 
     utils_to_import = (
-        "orbital_mechanics_utils.jl", "input_preparation.jl", "models.jl", "metrics.jl", "plots.jl", "loss_functions.jl", "output.jl", "nn_models.jl",
-        "create_ode_problems.jl", "plot_conditions.jl"
+        "orbital_mechanics/orbital_mechanics_utils.jl", "preprocessing/input_preparation.jl", "models/models.jl", "metrics.jl", "plots/plots.jl", "loss_functions/loss_functions.jl", "output/output.jl", "models/nn_models.jl",
+        "preprocessing/create_ode_problems.jl", "plots/plot_conditions.jl", "preprocessing/process_datasets.jl"
     )
 
     for util in utils_to_import
         include(utils_path * util);
     end
 
-end
-
-
-function process_datasets(datasets)
-
-    true_waveform_array = []
-    tsteps_array = []
-    tspan_array = []
-    model_params_array = []
-    u0_array = []
-    dt_data_array = []
-
-    for data in datasets
-        push!(true_waveform_array, data["waveform"])
-        push!(tsteps_array, data["tsteps"])
-        push!(tspan_array, data["tspan"])
-        push!(model_params_array, data["model_params"])
-        push!(u0_array, data["u0"])
-        push!(dt_data_array, data["dt_data"])
-    end
-
-    processed_data = Dict("true_waveform" => true_waveform_array, "tsteps" => tsteps_array, "tspan" => tspan_array, "model_params" => model_params_array, "u0" => u0_array, "dt_data" => dt_data_array)
-    return processed_data
 end
